964c794c728efd4b97d29e0888fa7b31ebb42841
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-09 17:31:09 +0300

    Creating the needed tables and migrating

--- E-Commerce-BackEnd-Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ E-Commerce-BackEnd-Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('dob');
+            $table->string('country');
+            $table->string('city');
+            $table->string('phone');
+            $table->string('gender');
+            $table->string('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
