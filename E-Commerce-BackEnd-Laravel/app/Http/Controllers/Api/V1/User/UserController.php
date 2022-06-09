50b398e7baccb0c9a4d1f7e5e3f8e5303b440ba3
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-09 22:29:44 +0300

    Adding versioning system to the project and controlling it by a middleware

---
+++ E-Commerce-BackEnd-Laravel/app/Http/Controllers/Api/V1/User/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\User;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller{
+    
+    
+
+}
---
+++ E-Commerce-BackEnd-Laravel/routes/api_v1.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\V1\User\UserController;
+
+
+    Route::group(['prefix' => 'user'] , function(){
+
+    });
+
