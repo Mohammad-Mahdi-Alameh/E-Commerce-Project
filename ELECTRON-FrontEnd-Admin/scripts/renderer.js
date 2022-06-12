3a50db0232fe79f5f7c8b2b5c3070ce7f4c70f28
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-13 01:40:54 +0300

    Preparing the stage for axious to link the frontend with the login api

--- ELECTRON-FrontEnd-Admin/scripts/renderer.js
+++ ELECTRON-FrontEnd-Admin/scripts/renderer.js
@@ -1,10 +1,14 @@
-let login = document.getElementById("login");
+
+
+
+let login_btn = document.getElementById("login");
 let form = document.getElementById("login-form");
 let column = document.getElementById("column");
 let login_form_button = document.getElementById("btn-form-login");
 let cancel = document.getElementById("btn-form-cancel");
 
-login.addEventListener("click", function(event){
+
+login_btn.addEventListener("click", function(event){
 
     
     event.preventDefault();
@@ -12,7 +16,7 @@ login.addEventListener("click", function(event){
 
 });
 
-login.addEventListener("click", function(event){
+login_form_button.addEventListener("click", function(event){
 
     
     event.preventDefault();
@@ -39,5 +43,37 @@ function hideLoginForm() {
 
     form.style.display="none";
     column.style.display="block";
+    
+}
+
+function login(){
+
+    const username = document.getElementById("username").value;
+
+    const password = document.getElementById("password").value;
+
+    if (username == "" || password == ""){
+
+        alert("Please fill the missing fields!")
+    }
+    
+    
+    let data = new FormData();
+
+    data.append('username', username);
+
+    data.append('password', password);
+
+    let url = 'http://127.0.0.1:8000/api/v1/admin/login';
+
+    axios({
+   
+        method: 'POST',
+   
+        url: url,
+   
+        data:data,
+   
+    })
     
 }
