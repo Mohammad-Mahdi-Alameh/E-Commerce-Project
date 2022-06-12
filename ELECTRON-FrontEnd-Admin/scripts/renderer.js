945f98e905bee70e34f35ef59506aaa5579eed77
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-13 00:50:11 +0300

    Creating a form for the admin to login

---
+++ ELECTRON-FrontEnd-Admin/scripts/renderer.js
@@ -0,0 +1,15 @@
+let login = document.getElementById("login");
+
+login.addEventListener("click", function(event){
+
+    
+    event.preventDefault();
+    popUpLoginForm();
+
+});
+
+function popUpLoginForm(){
+
+
+    
+}
