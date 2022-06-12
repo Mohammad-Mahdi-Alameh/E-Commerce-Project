6654ccce56861e69b1d4566ac434981a3d337790
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-13 01:23:22 +0300

    Hiding the form when cancel is clicked

--- ELECTRON-FrontEnd-Admin/scripts/renderer.js
+++ ELECTRON-FrontEnd-Admin/scripts/renderer.js
@@ -1,4 +1,8 @@
 let login = document.getElementById("login");
+let form = document.getElementById("login-form");
+let column = document.getElementById("column");
+let login_form_button = document.getElementById("btn-form-login");
+let cancel = document.getElementById("btn-form-cancel");
 
 login.addEventListener("click", function(event){
 
@@ -8,8 +12,32 @@ login.addEventListener("click", function(event){
 
 });
 
+login.addEventListener("click", function(event){
+
+    
+    event.preventDefault();
+    login();
+
+});
+
+cancel.addEventListener("click", function(event){
+
+    
+    event.preventDefault();
+    hideLoginForm();
+
+});
+
 function popUpLoginForm(){
 
+    form.style.display="block";
+    column.style.display="none";
 
+}
+
+function hideLoginForm() {
+
+    form.style.display="none";
+    column.style.display="block";
     
 }
