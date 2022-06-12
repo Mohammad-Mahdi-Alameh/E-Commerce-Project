720b84c0fc981df8cd9362c6d32826f2354e3a56
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-12 23:28:43 +0300

    Linking the view_items with the api get_items and displaying them on screen

--- User-FrontEnd/scripts/index.js
+++ User-FrontEnd/scripts/index.js
@@ -1,57 +1,57 @@
 ////
-window.onload=viewCategories;
+window.onload = viewCategories;
 ////
 
-let flex_container=document.getElementById("flex-container");
+let flex_container = document.getElementById("flex-container");
 
 
 function viewCategories() {
-    
-  
-   
+
+
+
     var url = 'http://127.0.0.1:8000/api/v1/user/get_categories';
 
     axios({
-   
+
         method: 'GET',
-   
+
         url: url,
-   
-   
+
+
     })
 
-    .then(function (response) {
-   
-        let result = response.data;
-        
-        var string="";
+        .then(function (response) {
 
-        result.forEach(element=>{
-                string += '<div class="flex-item" id='+ element.name +'><ul class="zoom">'
-                
-                                +'<li>' + element.name + '</li>' 
-                                
-                                +'</ul></div>';
-                                
-    });
+            let result = response.data;
 
-    flex_container.innerHTML=string; 
-    let flex_items=document.getElementsByClassName("flex-item");
-    for (let i = 0; i < flex_items.length; i++) {
-        
-        // flex_items[i].addEventListner("click",viewItems);
-        flex_items[i].addEventListener("click", function(event){
-    
-            // console.log(this);
-            event.preventDefault();
-            var category_name = this.id;
-            // console.log(category_name);
-            window.location.href = "./pages/view_items.html?category_name=" +category_name;
-        
-    }
-        );
-    }
-});}
+            var string = "";
+
+            result.forEach(element => {
+                string += '<div class="flex-item" id=' + element.name + '><ul class="zoom">'
+
+                    + '<li>' + element.name + '</li>'
+
+                    + '</ul></div>';
+
+            });
+
+            flex_container.innerHTML = string;
+            let flex_items = document.getElementsByClassName("flex-item");
+            for (let i = 0; i < flex_items.length; i++) {
+
+                // flex_items[i].addEventListner("click",viewItems);
+                flex_items[i].addEventListener("click", function (event) {
+
+                    // console.log(this);
+                    event.preventDefault();
+                    var category_name = this.id;
+                    // console.log(category_name);
+                    window.location.href = "./pages/view_items.html?category_name=" + category_name;
+
+                });
+            }
+        });
+}
 
 
-            
+
---
+++ User-FrontEnd/scripts/view_items.js
@@ -0,0 +1,61 @@
+////
+window.onload = viewItems;
+////
+
+
+let flex_container = document.getElementById("flex-container");
+
+
+
+
+function viewItems() {
+    let urlString = window.location.href;
+    let paramString = urlString.split('?')[1];
+    let queryString = new URLSearchParams(paramString);
+    for (var pair of queryString.entries()) {
+        console.log(pair[1]);
+    }
+
+    var url = 'http://127.0.0.1:8000/api/v1/user/get_items/' + pair[1];
+
+    axios({
+
+        method: 'GET',
+
+        url: url,
+
+
+    })
+
+        .then(function (response) {
+
+            let result = response.data;
+
+            var string = "";
+
+            result.forEach(element => {
+                
+                string += '<div class="flex-item" id=' + element.id + '><ul class="zoom">'
+
+                    + '<li>' + element.name + '</li>'
+                    + '<li>' + element.price + '</li>'
+                    + '<li>' + element.trademark + '</li>'
+                    + '<li>' + element.model + '</li>'
+                    + '<li>' + element.image + '</li>'
+                    + '<li>' + element.description + '</li>'
+                    + '<li>' + element.usage + '</li>'
+
+                    + '</ul></div>';
+
+            });
+
+            flex_container.innerHTML = string;
+
+
+        });
+
+
+}
+
+
+
